486206f2d5865f0727c71d737e6fd8e7fdf6a425
omersusin <tekgercek73@gmail.com>
2026-07-09 00:48:33 +0000

    fix: add missing background import in RootScreen

--- app/src/main/java/co/coffeery/app/ui/screens/root/RootScreen.kt
+++ app/src/main/java/co/coffeery/app/ui/screens/root/RootScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
