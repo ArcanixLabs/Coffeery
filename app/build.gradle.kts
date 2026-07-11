cfa9fa0cd87f90450c76a5a4b45f6daee90d0fea
omersusin <tekgercek73@gmail.com>
2026-07-11 18:21:15 +0000

    release: v3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "co.coffeery.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "3.0.0"
+        versionCode = 6
+        versionName = "3.0.1"
         vectorDrawables { useSupportLibrary = true }
     }
 
