6b3eac0109a8c348469ba0f9213a872f2f7aa63d
omersusin <tekgercek73@gmail.com>
2026-07-12 15:40:39 +0000

    feat: add real Google OAuth Web Client ID for sign-in

--- app/src/main/java/co/coffeery/app/util/CloudBackupManager.kt
+++ app/src/main/java/co/coffeery/app/util/CloudBackupManager.kt
@@ -2,6 +2,7 @@ package co.coffeery.app.util
 
 import android.content.Context
 import android.content.Intent
+import co.coffeery.app.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -22,6 +23,7 @@ class CloudBackupManager(private val context: Context) {
         val builder = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestProfile()
+            .requestIdToken(context.getString(R.string.google_server_client_id))
         return GoogleSignIn.getClient(context, builder.build())
     }
 
